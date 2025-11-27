e1341a71db2c7c04a6d590277ece77b34547593b
Maxwell <122761335+MaxwellMoore@users.noreply.github.com>
2025-11-27 11:44:54 -0700

    Added 'jk' and 'kj' keymaps.

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,12 +1,52 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+--[[
+  ----------------------------------------------------------
+  DEFAULT INFO (LazyVim):
+  ----------------------------------------------------------
+    - Keymaps are automatically loaded on the VeryLazy event
+    - Default keymaps that are always set:
+        https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+    - Add any additional keymaps here
 
--- vim.keymap.set("n", "<leader>sx", require("telescope.builtin").resume, {
---   noremap = true,
---   silent = true,
--- })
+  ----------------------------------------------------------
+  KEYMAP ANATOMY (vim.keymap.set):
+  ----------------------------------------------------------
 
--- lua/config/keymaps.lua
+    --------------------------------------------------------
+    vim.keymap.set(mode, lhs, rhs, opts)
 
-vim.keymap.set("i", "jk", "<Esc>", { noremap = true, silent = true, desc = "Exit insert mode with jk" })
+    --------------------------------------------------------
+    mode: The Vim mode the mapping applies to.
+      - "i" = insert mode
+      - "n" = normal mode
+      - "v" = visual mode
+      - "x" = visual block mode
+      - "t" = terminal mode
+      Can also be a table, e.g. { "n", "v" }
+
+    --------------------------------------------------------
+    lhs: The key sequence to trigger the mapping (e.g. "jk").
+
+    --------------------------------------------------------
+    rhs: The action executed when lhs is pressed.
+      - "<Esc>" simulates pressing the Escape key.
+
+    --------------------------------------------------------
+    opts: A table of options for the mapping.
+      - noremap = true  -> prevents recursive remapping.
+      - silent = true   -> suppresses command output.
+      - desc = "..."    -> description shown by plugins like which-key.
+
+  ----------------------------------------------------------
+  Example:
+    vim.keymap.set("i", "jk", "<Esc>", { noremap = true, silent = true, desc = "Exit insert mode" })
+
+]]
+
+-- Exit insert mode with "jk" or "kj".
+for _, combo in ipairs({ "jk", "kj" }) do
+  vim.keymap.set("i", combo, "<Esc>", {
+    noremap = true,
+    silent = true,
+    desc = "Exit insert mode with " .. combo,
+  })
+end
